53add11a6f6c29544b0c0473944d859f8eb9a1d4
gwendalpenvern <gwendal.penvern@gmail.com>
2020-11-05 16:02:55 +0100

    modifying the name type of the graph image and his generation and printing

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template
+import datetime
 import os
 
 app = Flask(__name__)
@@ -21,6 +22,6 @@ def add_header(r):
 
 @app.route("/")
 def affTemp():
-    return render_template('index.html', image="test.png")
+    return render_template('index.html', path_image="/static/images/" + str(datetime.datetime.now())[:10] + ".png" )
 
 app.run(debug=True, host='0.0.0.0', port=5000)
